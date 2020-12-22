a61b83427d3b4a8fe229129fa2bbfb0d14637ac3
huyudong <379145942@qq.com>
2020-12-22 11:11:54 +0800

    'init'

---
+++ src/components/test.vue
@@ -0,0 +1,33 @@
+<template>
+    <div v-if="show">
+      111111111
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'test',
+    data(){
+      return{
+        show:false
+      }
+    },
+    created () {
+      this.show=true;
+      console.log(2222222222)
+    },
+    watch:{
+      $route(){
+        this.show=false;
+        this.$nextTick(v=>{
+          this.show=true;
+          console.log(7777777)
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
